4bb5b6a6967202fb7ca93336b5d0cdbfc8926f59
Louis Yupiter Consolatio <consolatio123@gmail.com>
2020-12-11 14:15:54 +0700

    fix dist

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static("./dist/belajarangular"));
+app.use(express.static("./dist/belajarangular/belajarangular"));
 
 app.get("/*", function (req, res) {
-  res.sendFile("index.html", { root: "dist/belajarangular/" });
+  res.sendFile("index.html", { root: "dist/belajarangular/belajarangular/" });
 });
 
 app.listen(process.env.PORT || 4200);
